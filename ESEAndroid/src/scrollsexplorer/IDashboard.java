b7a844f2c1100e12bcd2c21996dafb7e6d63d05f
phil <phil@Thinky>
2016-02-10 00:50:09 +0100

    Most non awt gear moved from ElderScrollsExplorer to ESEAndroid

---
+++ ESEAndroid/src/scrollsexplorer/IDashboard.java
@@ -0,0 +1,21 @@
+package scrollsexplorer;
+
+import scrollsexplorer.simpleclient.physics.PhysicsSystem;
+
+//FIXME: what a rubbish system we have here
+public abstract class IDashboard
+{
+	public static IDashboard dashboard;
+
+	public abstract void setPhysicSystem(PhysicsSystem physicsSystem);
+
+	public abstract void setEsmLoading(int isLoading);
+
+	public abstract void setCellLoading(int isLoading);
+
+	public abstract void setNearLoading(int isLoading);
+
+	public abstract void setFarLoading(int isLoading);
+
+	public abstract void setLodLoading(int isLoading);
+}
