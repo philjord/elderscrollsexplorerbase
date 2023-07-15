2a9e03d3bc34ee30d819221abf5d203f0cdca91f
Philip Jordan <p.j.nz@outlook.com>
2023-07-15 18:40:37 +1200

    it's a bit more like phone findadoor is in

--- ElderScrollsExplorerBase/src/scrollsexplorer/ScrollsExplorerNewt.java
+++ ElderScrollsExplorerBase/src/scrollsexplorer/ScrollsExplorerNewt.java
@@ -2,7 +2,9 @@ package scrollsexplorer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.jogamp.java3d.Transform3D;
 import org.jogamp.java3d.compressedtexture.CompressedTextureLoader;
 import org.jogamp.vecmath.Quat4f;
 import org.jogamp.vecmath.Vector3f;
@@ -18,11 +20,17 @@ import bsa.source.BsaSoundSource;
 import bsa.source.BsaTextureSource;
 import bsaio.ArchiveFile;
 import bsaio.BSArchiveSetFile;
+import esmio.common.data.plugin.PluginGroup;
+import esmio.common.data.record.Record;
 import esmio.loader.ESMManager;
 import esmio.loader.ESMManagerFile;
 import esmio.loader.IESMManager;
+import esmio.utils.ESMUtils;
 import esmio.utils.source.EsmSoundKeyToName;
+import esmj3d.data.shared.records.CommonREFR;
+import esmj3d.data.shared.subrecords.XTEL;
 import esmj3d.j3d.BethRenderSettings;
+import esmj3d.j3d.cell.J3dICellFactory;
 import esmj3d.j3d.j3drecords.inst.J3dLAND;
 import javaawt.VMEventQueue;
 import javaawt.image.VMBufferedImage;
@@ -38,6 +46,7 @@ import scrollsexplorer.simpleclient.BethWorldVisualBranch;
 import scrollsexplorer.simpleclient.SimpleBethCellManager;
 import scrollsexplorer.simpleclient.SimpleWalkSetup;
 import scrollsexplorer.simpleclient.SimpleWalkSetupInterface;
+import scrollsexplorer.simpleclient.mouseover.ActionableMouseOverHandler;
 import scrollsexplorer.simpleclient.physics.DynamicsEngine;
 import scrollsexplorer.simpleclient.physics.PhysicsSystem;
 import scrollsexplorer.simpleclient.tes3.Tes3Extensions;
@@ -66,7 +75,7 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 
 	public BSArchiveSetFile bsaFileSet;
 
-	private GameConfig selectedGameConfig = null;
+	private GameConfig gameConfigToLoad = null;
 
 	private Tes3Extensions tes3Extensions;
 
@@ -87,38 +96,32 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 		ArchiveFile.USE_MINI_CHANNEL_MAPS = true;
 		ArchiveFile.USE_NON_NATIVE_ZIP = false;
 
+		//TODO this client should act EXACTLY like the phone one including find doors and the graphics config set up
+		// of ScrollExplorer
+		// also it should use a seperate bsa collection to ensure it does what the phoen does
 		// these 5 test the "no dds support" issue and solution on phones
 		BsaTextureSource.allowedTextureFormats = BsaTextureSource.AllowedTextureFormats.DDS;
 		CompressedTextureLoader.RETURN_DECOMPRESSED_DDS = true;
+		CompressedTextureLoader.DROP_0_MIP = true;
 		javaawt.image.BufferedImage.installBufferedImageDelegate(VMBufferedImage.class);
 		javaawt.imageio.ImageIO.installBufferedImageImpl(VMImageIO.class);
 		javaawt.EventQueue.installEventQueueImpl(VMEventQueue.class);		
 
-		BethRenderSettings.setFarLoadGridCount(4);
-		BethRenderSettings.setNearLoadGridCount(2);
-		BethRenderSettings.setLOD_LOAD_DIST_MAX(32);
-		BethRenderSettings.setObjectFade(100);
-		BethRenderSettings.setItemFade(100);
-		BethRenderSettings.setActorFade(50);
+ 
+		
 		BethWorldVisualBranch.LOAD_PHYS_FROM_VIS = false;
 		DynamicsEngine.MAX_SUB_STEPS = 2;
 		PhysicsSystem.MIN_TIME_BETWEEN_STEPS_MS = 40;
 		NiGeometryAppearanceFactoryShader.setAsDefault();
 		ShaderSourceIO.ES_SHADERS = false;// not the same!
 		J3dNiTriBasedGeom.USE_FIXED_BOUNDS = true;
-		// this definately doesn't help on desktop, but lots of methods calls so maybe?
+		// this definitely doesn't help on desktop, but lots of methods calls so maybe?
 		NifCharacter.BULK_BUFFER_UPDATES = false;
 
 		NiGeometryAppearanceFactoryShader.setAsDefault();
 		CompressedTextureLoader.setAnisotropicFilterDegree(4);
 
-		//for big games go low spec
-		if (!gameToLoad.equals("TESIII: Morrowind"))
-		{
-			BethRenderSettings.setFarLoadGridCount(0);
-			BethRenderSettings.setNearLoadGridCount(2);
-			BethRenderSettings.setLOD_LOAD_DIST_MAX(0);
-		}
+ 
 
 		try
 		{
@@ -238,18 +241,18 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 	 */
 	private void setSelectedGameConfig(GameConfig newGameConfig)
 	{
-		selectedGameConfig = newGameConfig;
-		simpleWalkSetup.getAvatarCollisionInfo().setAvatarYHeight(selectedGameConfig.avatarYHeight);
+		gameConfigToLoad = newGameConfig;
+		simpleWalkSetup.getAvatarCollisionInfo().setAvatarYHeight(gameConfigToLoad.avatarYHeight);
 
 		Thread t = new Thread() {
 			@Override
 			public void run()
 			{
-				synchronized (selectedGameConfig)
+				synchronized (gameConfigToLoad)
 				{
 					IDashboard.dashboard.setEsmLoading(1);
 
-					esmManager = ESMManagerFile.getESMManager(selectedGameConfig.getESMPath());
+					esmManager = ESMManagerFile.getESMManager(gameConfigToLoad.getESMPath());
 					bsaFileSet = null;
 					if (esmManager != null)
 					{
@@ -264,13 +267,13 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 						YawPitch yp = YawPitch
 								.parse(PropertyLoader.properties.getProperty("YawPitch" + esmManager.getName(), new YawPitch().toString()));
 						Vector3f trans = PropertyCodec.vector3fOut(PropertyLoader.properties.getProperty("Trans" + esmManager.getName(),
-								selectedGameConfig.startLocation.toString()));
+								gameConfigToLoad.startLocation.toString()));
 						int prevCellformid = Integer.parseInt(PropertyLoader.properties.getProperty("CellId" + esmManager.getName(), "-1"));
 						simpleWalkSetup.getAvatarLocation().set(yp.get(new Quat4f()), trans);
 
 						if (prevCellformid == -1)
 						{
-							prevCellformid = selectedGameConfig.startCellId;
+							prevCellformid = gameConfigToLoad.startCellId;
 						}
 
 						new EsmSoundKeyToName(esmManager);
@@ -280,7 +283,7 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 
 						if (bsaFileSet == null)
 						{
-							bsaFileSet = new BSArchiveSetFile(new String[] { selectedGameConfig.scrollsFolder }, true);
+							bsaFileSet = new BSArchiveSetFile(new String[] { gameConfigToLoad.scrollsFolder }, true);
 						}
 
 						if (bsaFileSet.size() == 0)
@@ -298,6 +301,54 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 						BgsmSource.setBgsmSource(meshSource);
 
 						mediaSources = new MediaSources(meshSource, textureSource, soundSource);
+						
+						
+						 // do we have some ktx images or are we stuck with slow big dds decompress
+                        boolean ddsDecompressing = !((BsaTextureSource) textureSource).hasKTX() && !((BsaTextureSource) textureSource).hasASTC();
+                        // this is terrible 1/4 images, but helps if no KTX files are available and we are decompressing dds
+                        if (ddsDecompressing)
+                            CompressedTextureLoader.DROP_0_MIP = true;
+
+                        if (gameConfigToLoad.folderKey.equals("MorrowindFolder")) {
+
+                            BethRenderSettings.setFarLoadGridCount(8);
+                            BethRenderSettings.setNearLoadGridCount(2);
+                            BethRenderSettings.setLOD_LOAD_DIST_MAX(32);
+                            BethRenderSettings.setObjectFade(150);
+                            BethRenderSettings.setItemFade(120);
+                            BethRenderSettings.setActorFade(50);
+                            BethRenderSettings.setFogEnabled(false);
+                            //BethWorldVisualBranch.FOG_START = 100;
+                            //BethWorldVisualBranch.FOG_END = 250;
+
+                            //long distance view
+                            //BethRenderSettings.setFarLoadGridCount(16);
+                            //
+                        } else {
+
+                            //TODO: must make a per game setting recorder for this gear!
+
+                            //oblivion goes hard, others are cautious for now
+                            if (gameConfigToLoad.folderKey.equals("OblivionFolder")) {
+                                BethRenderSettings.setFarLoadGridCount(4);
+                                BethRenderSettings.setNearLoadGridCount(1);
+                                BethRenderSettings.setLOD_LOAD_DIST_MAX(32);
+                                BethRenderSettings.setObjectFade(100);
+                                BethRenderSettings.setItemFade(80);
+                                BethRenderSettings.setActorFade(35);
+                                BethWorldVisualBranch.FOG_START = 150;
+                                BethWorldVisualBranch.FOG_END = 350;
+                            }  else  {
+                                BethRenderSettings.setFarLoadGridCount(3);
+                                BethRenderSettings.setNearLoadGridCount(1);
+                                BethRenderSettings.setLOD_LOAD_DIST_MAX(16);
+                                BethRenderSettings.setObjectFade(50);
+                                BethRenderSettings.setItemFade(60);
+                                BethRenderSettings.setActorFade(35);
+                                BethWorldVisualBranch.FOG_START = 75;
+                                BethWorldVisualBranch.FOG_END = 150;
+                            }
+                        }
 
 						simpleWalkSetup.configure(meshSource, simpleBethCellManager);
 						simpleWalkSetup.setEnabled(false);
@@ -340,15 +391,22 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 						});
 
 						// I could use the j3dcellfactory now? with the cached cell records?
-						simpleBethCellManager.setSources(selectedGameConfig, esmManager, mediaSources);
+						simpleBethCellManager.setSources(gameConfigToLoad, esmManager, mediaSources);
 
-						if (selectedGameConfig == GameConfig.allGameConfigs.get(0))
+						if (gameConfigToLoad == GameConfig.allGameConfigs.get(0))
 						{
 							System.out.println("Adding Tes3 extensions");
-							tes3Extensions = new Tes3Extensions(selectedGameConfig, esmManager, mediaSources, simpleWalkSetup,
+							tes3Extensions = new Tes3Extensions(gameConfigToLoad, esmManager, mediaSources, simpleWalkSetup,
 									simpleBethCellManager);
 						}
 
+						
+                        // need to set Trans to a door somewhere?
+                        findADoor(prevCellformid, yp, trans);
+                        
+                        simpleWalkSetup.getAvatarLocation().set(yp.get(new Quat4f()), trans);
+                               
+                        
 						display(prevCellformid);
 
 					}
@@ -428,5 +486,127 @@ public class ScrollsExplorerNewt implements BethRenderSettings.UpdateListener, L
 
 		new ScrollsExplorerNewt(args[0]);
 	}
+	
+	
+	private void findADoor(int formToLoad, YawPitch yp, Vector3f trans) {
+        J3dICellFactory j3dCellFactory = gameConfigToLoad.j3dCellFactory;
+        if (j3dCellFactory != null) {
+
+            ArrayList<CommonREFR> doors = new ArrayList<>();
+            if (gameConfigToLoad.gameName != "TESIII: Morrowind") {
+                // if SimpleBethCellManager.setSources has been called the persistent children will have been loaded
+                PluginGroup cellChildGroups = j3dCellFactory.getPersistentChildrenOfCell(formToLoad);
+                for (Record record : cellChildGroups.getRecordList()) {
+                    // is this a door way?
+                    if (record.getRecordType().equals("REFR")) {
+                        // don't go game specific just the common data needed (which include XTEL!)
+                        CommonREFR commonREFR = new CommonREFR(record, true);
+                        XTEL xtel = commonREFR.XTEL;
+                        //if we are a door outward we have a door inward
+                        if (xtel != null) {
+                            Record otherDoor;
+                            if (xtel.doorFormId != 0) {
+                                otherDoor = j3dCellFactory.getRecord(xtel.doorFormId);
+                                if (otherDoor != null) {
+                                    CommonREFR otherDoorCommonREFR = new CommonREFR(otherDoor, true);
+                                    doors.add(otherDoorCommonREFR);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            } else {
+                if (formToLoad == 0) {
+                    int attempts = 0;
+                    while (doors.size() == 0 && attempts < 20) {
+                        attempts++;
+                        // morrowind itself we'll have to pick a random cell from -25 to +25 until we find a door
+                        ///	looks like x = 23 to -18 y is 27 to -17
+                        int x = (int) ((Math.random() * 41) - 18);
+                        int y = (int) ((Math.random() * 44) - 17);
+                        try {
+                            PluginGroup cellChildGroups = esmManager.getWRLDExtBlockCELLChildren(0, x, y);
+                            if (cellChildGroups != null) {
+                                for (Record record : ESMUtils.getChildren(cellChildGroups, PluginGroup.CELL_TEMPORARY)) {
+                                    // is this a door way?
+                                    if (record.getRecordType().equals("REFR")) {
+                                        //TODO: Fro Morrowind I need to find all doors elsewhere and see where they wouldput you in this cell
+                                        // which is obviously too much work!
+
+                                        // morrowind has a half pie system using DNAM
+                                        // morrowind has no match inwards door so we'll have to make up t and yp
+                                        esmj3dtes3.data.records.REFR commonREFR = new esmj3dtes3.data.records.REFR(record);
+                                        XTEL xtel = commonREFR.XTEL;// xtel describe the target position and DNAM states the cell, but a NULL DNAM is Morrowind (id=0)
+                                        if (xtel != null)
+                                            doors.add(commonREFR);
+                                    }
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return;
+                        }
+                    }
+
+                } else {
+                    // no persistent system, just load them all!
+                    PluginGroup cellChildGroups = j3dCellFactory.getPersistentChildrenOfCell(formToLoad);
+                    if (cellChildGroups != null) {
+                        for (Record record : ESMUtils.getChildren(cellChildGroups, PluginGroup.CELL_TEMPORARY)) {
+                            // is this a door way?
+                            if (record.getRecordType().equals("REFR")) {
+                                //TODO: Fro Morrowind I need to find all doors elsewhere and see where they wouldput you in this cell
+                                // which is obviously too much work!
+
+                                // morrowind has a half pie system using DNAM
+                                // morrowind has no match inwards door so we'll have to make up t and yp
+                                esmj3dtes3.data.records.REFR commonREFR = new esmj3dtes3.data.records.REFR(record);
+                                XTEL xtel = commonREFR.XTEL;// xtel describe the target position and DNAM states the cell, but a NULL DNAM is Morrowind (id=0)
+                                if (xtel != null)
+                                    doors.add(commonREFR);
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            if (doors.size() > 0) {
+                int idx = (int) (Math.random() * (doors.size() - 1));
+                if (gameConfigToLoad.gameName != "TESIII: Morrowind") {
+                    XTEL xtel = doors.get(idx).XTEL; // note this is the other door so the exit is right but it's cell is not our cell
+                    Vector3f t = ActionableMouseOverHandler.getTrans(xtel.x, xtel.y, xtel.z);
+                    t.y += 1; // TODO: cos it's the floor I reckon, nay something off in all direction a bit here
+                    Quat4f r = ActionableMouseOverHandler.getRot(xtel.rx, xtel.ry, xtel.rz);
+
+
+
+
+                    trans.set(t);
+                    yp.set(r);
+                } else {
+                    esmj3dtes3.data.records.REFR refr = (esmj3dtes3.data.records.REFR) doors.get(idx);
+                    Vector3f loc = refr.getTrans();
+                    Vector3f rot = refr.getEulerRot();
+                    //TODO:  location needs to be pushed forward in facing as this is just the door itself
+                    Vector3f t = ActionableMouseOverHandler.getTrans(loc.x, loc.y, loc.z);
+                    //t.y += 1; // TODO: cos it's the floor I reckon, nay something off in all direction a bit here
+                    Quat4f r = ActionableMouseOverHandler.getRot(rot.x, rot.y, rot.z);
+
+
+                    // now push forward by 1 meter to see if we are in front of the door
+                    Transform3D t3d = new Transform3D(r,new Vector3f(0,0,0), 1f);
+                    Vector3f f = new Vector3f(0,0,1);
+                    t3d.transform(f);
+                    t.add(f);
+
+                    trans.set(t);
+                    yp.set(r);
+                }
+            }
+        }
+
+    }
 
 }
