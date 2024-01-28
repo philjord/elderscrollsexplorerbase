f866df3454c3feafda20b997acd95dfdf5c22c4f
Philip Jordan <p.j.nz@outlook.com>
2024-01-29 11:40:14 +1300

    Starfield added

--- ElderScrollsExplorerBase/src/scrollsexplorer/GameConfig.java
+++ ElderScrollsExplorerBase/src/scrollsexplorer/GameConfig.java
@@ -89,8 +89,6 @@ public class GameConfig {
 					"Oblivion.esm", //
 					"oblivion", 186689, new Vector3f(57, 80, 0), new YawPitch()));
 
-			//Android TESIV: Oblivion = 143176, (425,43,-912)
-
 			allGameConfigs.add(new GameConfig("FO3: Fallout 3", //
 					new esmj3dfo3.j3d.cell.J3dCellFactory(), //
 					"textures\\sky\\urbancloudovercastlower01.dds", //
@@ -101,8 +99,6 @@ public class GameConfig {
 					"FallOut3Folder", //
 					"Fallout3.esm", //
 					"fallout3", 60, new Vector3f(-162, 182, 155), new YawPitch()));
-
-			//Android FO3: Fallout 3 = 2676, (-37, 165, 281)
 
 			allGameConfigs.add(new GameConfig("FONV: Fallout New Vegas", //
 					new esmj3dfo3.j3d.cell.J3dCellFactory(), //
@@ -123,11 +119,11 @@ public class GameConfig {
 						"meshes\\loadscreenart\\loadscreengiant01.nif", //
 						"meshes\\loadscreenart\\loadscreennoblebeddouble02.nif", //
 						"meshes\\loadscreenart\\loadscreenswampdragon1.nif"}, //
+					//FIXME, allow for a folder only and grab any of the nifs in there
 					1.8f, //
 					"SkyrimFolder", //
 					"Skyrim.esm", //
-					"skyrim", 60, new Vector3f(346, 256, 1459), new YawPitch()));
-			//Android TESV: Skyrim = 107119, (251, -44, 94)
+					"skyrim", 60, new Vector3f(346, 256, 1459), new YawPitch()));//FIXME: default location is not useful, possibly a intro point
 
 			allGameConfigs.add(new GameConfig("FO4: Fallout 4", //
 					new esmj3dfo4.j3d.cell.J3dCellFactory(), //
@@ -136,13 +132,20 @@ public class GameConfig {
 						"meshes\\LoadScreenArt\\CreatureDeathclawRunForward.nif", //
 						"meshes\\LoadScreenArt\\CryoPodLoadScreenArt01.nif", //
 						"meshes\\LoadScreenArt\\LS_WorkstationArmorB01.nif", //
-						"meshes\\LoadScreenArt\\VaultExitLoadScreen.nif"}, //
+						"meshes\\LoadScreenArt\\VaultExitLoadScreen.nif"}, //					
 					1.8f, //
 					"FallOut4Folder", //
 					"Fallout4.esm", //
 					"fallout4", 3988, new Vector3f(40, -18, -10), new YawPitch()));
 
-			//Android FO4: Fallout 4 = 7768, (19, 1, 5)
+			allGameConfigs.add(new GameConfig("STAR: Starfield", //
+					new esmj3dfo4.j3d.cell.J3dCellFactory(), //FIXME
+					"textures\\sky\\skyrimcloudsupper04.dds", //hahhah!
+					new String[] {"meshes\\LoadScreenArt\\Armor01PowerArmor1.nif" }, //					
+					1.8f, //
+					"StarfieldFolder", //
+					"Starfield.esm", //
+					"starfield", 3988, new Vector3f(40, -18, -10), new YawPitch()));
 
 			allGameConfigs.add(new GameConfig("Hunter Sneaker", //
 					new esmj3dtes4.j3d.cell.J3dCellFactory(), //
@@ -154,41 +157,4 @@ public class GameConfig {
 					"huntersneaker", 60, new Vector3f(0, 200, 0), new YawPitch()));
 		}
 	}
-	//morrowind
-	//meshes\i\in_v_arena.nif
-	//meshes\i\in_de_shipwreckul_lg.nif
-	//meshes\i\in_dae_hall_l_stair_curve_01.nif
-
-	// obliv
-	//meshes\dungeons\chargen\prisonhall04.nif
-	//meshes\dungeons\cloudrulertemple\testcloudrulerint.nif
-	//meshes\dungeons\cathedral\cathedralstenintback01.nif
-
-	//FO3 + FONV
-	//meshes\interface\loading\loadinganim01.nif
-
-	//skyrim
-	// all from meshes\loadscreenart  with black background
-
-	//FO4
-	// all from meshes\loadscreenart  with black background
-
-	/**
-	 * 
-	 * public void setSources(IESMManager esmManager, MediaSources mediaSources) {
-	 * 
-	 * float version = esmManager.getVersion();
-	 * 
-	 * if (version == 0.94f) { if (esmManager.getName().equals("Skyrim.esm")) { j3dCellFactory = new
-	 * esmj3dtes5.j3d.cell.J3dCellFactory(esmManager, esmManager, mediaSources); } else {
-	 * 
-	 * j3dCellFactory = new esmj3dfo3.j3d.cell.J3dCellFactory(esmManager, esmManager, mediaSources); } } else if
-	 * (version == 1.32f) { j3dCellFactory = new esmj3dfo3.j3d.cell.J3dCellFactory(esmManager, esmManager,
-	 * mediaSources); } else if (version == 1.0f || version == 0.8f) { j3dCellFactory = new
-	 * esmj3dtes4.j3d.cell.J3dCellFactory(esmManager, esmManager, mediaSources); } else if (version == 1.2f) {
-	 * j3dCellFactory = new esmj3dtes3.j3d.cell.J3dCellFactory(esmManager, esmManager, mediaSources); } else {
-	 * System.out.println("Bad esm version! " + version + " in " + esmManager.getName()); }
-	 * 
-	 * //System.out.println("j3dCellFactory = " + j3dCellFactory); }
-	 */
 }
